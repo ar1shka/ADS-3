a4a92a45772216a04ff94edd3b2c300db3fc06ae
Арина <78840926+ar1shka@users.noreply.github.com>
2021-05-20 19:50:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,115 +1,90 @@
 #include <string>
 #include "tstack.h"
 
-int priority(char prior) {
-switch (prior) {
-case '(':
-return 0;
-break;
-case ')':
-return 1;
-break;
-case '+':
-return 2;
-break;
-case '-':
-return 2;
-break;
-case '/':
-return 3;
-break;
-case '*':
-return 3;
-break;
-default:
-return -1;
-}
-}
-int calculate(int first, int second, char chis) {
-switch (chis) {
-case '-':
-return first - second;
-break;
-case '+':
-return first + second;
-break;
-case '/':
-return first / second;
-break;
-case '*':
-return first * second;
-break;	
-default:
-return -1;
-}
+int p(char count) {
+    switch (count) {
+      case '(':
+          return 0;
+      case ')':
+          return 1;
+      case '+':
+      case '-':
+          return 2;
+      case '*':
+      case '/':
+          return 3;
+      default:
+          return -1;
+    }
 }
 std::string infx2pstfx(std::string inf) {
-Tstack <char> steck;
-char t = 0;
-char ch;
-int a;
-std::string str;
-for (int i = 0; i < inf.length(); i++) {
-ch = inf[i];
-a = priority(ch);
-if (a > -1) {
-if ((a == 0 || a > priority(t) || steck.isEmpty()) && ch != ')') {
-if (steck.isEmpty())
-t = ch;
-steck.push(ch);
-}
-else if (ch == ')') {
-while (stack.get() != '(') {
-str.push_back(stack.get());
-str.push_back(' ');
-stack.pop();
-}
-stack.pop();
-if (stack.isEmpty())
-t = 0;
-}
-else {
-while (!stack.isEmpty() && priority(stack.get()) >= a) {
-str.push_back(stack.get());
-str.push_back(' ');
-stack.pop();
-}
-if (stack.isEmpty())
-t = inf[i];
-stack.push(inf[i]);
-}
-}
-else {
-str.push_back(ch);
-str.push_back(' ');
-}
-}
-while (!stack.isEmpty()) {
-str.push_back(stack.get());
-str.push_back(' ');
-stack.pop();
-}
-str.erase(str.end() - 1, str.end());
-return str;
+    TStack<char> stack;
+    std::string r;
+
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            r += inf[i];
+            r += ' ';
+        } else if (inf[i] == '(') {
+            stack.push(inf[i]);
+        } else if (p(inf[i]) > p(stack.get()) || stack.isEmpty()) {
+            stack.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                r += stack.get();
+                r += ' ';
+                stack.pop();
+            }
+
+            if (stack.get() == '(') {
+               stack.pop();
+            }
+        } else {
+            while (!stack.isEmpty() && p(stack.get()) >= p(inf[i])) {
+                r += stack.get();
+                r += ' ';
+                stack.pop();
+            }
+
+            stack.push(inf[i]);
+        }
+    }
+
+    while (!stack.isEmpty()) {
+        r += stack.get();
+        r += ' ';
+        stack.pop();
+    }
+
+    while (r[r.length() - 1] == ' ') {
+        r = r.substr(0, r.length()-1);
+    }
+
+    return r;
 }
 int eval(std::string pst) {
-TStack <int> stack;
-int s;
-int f;
-int d;
-int otv;
-for (int i = 0; i < pst.size(); i++)
-{
-s = priority(pst[i]);
-if (s == -1) stack.push(pst[i] - 48);
-else {
-f = stack.get();
-stack.pop();
-d = stack.get();
-stack.pop();
-otv = calculate(f, d, pst[i]);
-stack.push(otv);
-}
-}
-return stack.get();
+    TStack<int> stack;
+
+    for (int i = 0; i < pst.length(); i++) {
+        if ((pst[i] >= '0') && (pst[i] <= '9')) {
+            stack.push(pst[i] - '0');
+        } else if (pst[i] != ' ') {
+            int second = stack.get();
+            stack.pop();
+            int first = stack.get();
+            stack.pop();
+
+            if (pst[i] == '-') {
+                stack.push(first - second);
+            } else if (pst[i] == '+') {
+                stack.push(first + second);
+            } else if (pst[i] == '*') {
+                stack.push(first * second);
+            } else {
+                stack.push(first / second);
+            }
+        }
+    }
+
+    return stack.get();
 }
